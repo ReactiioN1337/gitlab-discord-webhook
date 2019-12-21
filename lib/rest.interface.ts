ee1896d288213c3f13523c3bfd5278ab2a37b01f
ReactiioN <reactiion@haze-productions.com>
2019-12-21 22:36:19 +0100

    🎨 Added rest api response interface

---
+++ lib/rest.interface.ts
@@ -0,0 +1,11 @@
+/**
+ * @author      ReactiioN
+ * @copyright   2019, https://reactiion.net
+ * @license     MIT
+ */
+import { Response } from 'request'
+
+export interface ResponseInterface {
+  res: Response
+  body?: any
+}
