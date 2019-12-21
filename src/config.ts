d8842d86a3b0be4bdfb1d5ef1dc675d1bc5f6cd6
ReactiioN <reactiion@haze-productions.com>
2019-12-21 22:55:09 +0100

    🐛 Added missing return

--- src/config.ts
+++ src/config.ts
@@ -32,6 +32,7 @@ export const load = async (): Promise<ConfigInterface | undefined> => {
 
       // todo: migration
     }
+    return cfg
   } catch ({ syscall }) {
     if (syscall === 'open') {
       const cfg: ConfigInterface = {
@@ -44,7 +45,27 @@ export const load = async (): Promise<ConfigInterface | undefined> => {
         ],
         server: {
           port: 8080,
-          title: ''
+          title: '',
+          proxies: [
+            'loopback',
+            'linklocal',
+            'uniquelocal',
+            // https://www.cloudflare.com/ips-v4
+            '173.245.48.0/20',
+            '103.21.244.0/22',
+            '103.22.200.0/22',
+            '103.31.4.0/22',
+            '141.101.64.0/18',
+            '108.162.192.0/18',
+            '190.93.240.0/20',
+            '188.114.96.0/20',
+            '197.234.240.0/22',
+            '198.41.128.0/17',
+            '162.158.0.0/15',
+            '104.16.0.0/12',
+            '172.64.0.0/13',
+            '131.0.72.0/22'
+          ]
         }
       }
 
